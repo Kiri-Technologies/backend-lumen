ca04d127a18fa4cfbc6f1a7e6f31f925af54bdba
M. Faiz Triputra <m.faiztpaduhai@gmail.com>
2022-03-06 21:21:05 +0700

    token extended to 1 week

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ class Controller extends BaseController
         return response()->json([
             'token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60
+            'expires_in' => Auth::factory()->getTTL() * 10080
         ], 200);
     }
 }
